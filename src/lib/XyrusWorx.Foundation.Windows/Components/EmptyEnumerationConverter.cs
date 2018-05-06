6c56166feae194003a1fb039fe635619e884ae7f
Georg Kiehne <georg.kiehne@xyrus-worx.org>
2018-05-06 22:46:12 +0200

    Added EmptyEnumerationConverter and TriggerConverter

---
+++ src/lib/XyrusWorx.Foundation.Windows/Components/EmptyEnumerationConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+namespace XyrusWorx.Windows.Components
+{
+	[PublicAPI]
+	public class EmptyEnumerationConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => !(value.CastTo<IEnumerable>()?.OfType<object>().Any() ?? false);
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+	}
+
+}
---
+++ src/lib/XyrusWorx.Foundation.Windows/Components/TriggerConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using JetBrains.Annotations;
+
+namespace XyrusWorx.Windows.Components 
+{
+	[PublicAPI]
+	public class TriggerConverter : IValueConverter
+	{
+		public object Value { get; set; }
+		public object Result { get; set; }
+		
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => Equals(value, Value) ? Result : value;
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => throw new NotSupportedException();
+	}
+}
